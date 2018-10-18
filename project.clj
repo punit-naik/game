d8d5661e6b22f6a602690ff620f13622cdc301d8
Punit Naik <naik.punit44@gmail.com>
2018-10-18 23:35:49 +0530

    :art: Added project url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject game "1.0.0"
   :description "A Text Adventure Game in Clojure"
-  :url "http://example.com/FIXME"
+  :url "https://punit-naik.github.io/game"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]]
